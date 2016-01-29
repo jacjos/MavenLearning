2eb8f8a546d3ff1ce50be8c73e1588b1d6a627a7
jacjos <jacjos@gmail.com>
2016-01-29 12:21:12 +0530

    New module Initial Commit

---
+++ MavenLearning/MavenLearningPOM/MavenLearningPresentation/src/main/java/com/learning/presentation/LandingPresentation.java
@@ -0,0 +1,17 @@
+package com.learning.presentation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LandingPresentation {
+	
+	private static final Logger logger = LoggerFactory.getLogger(LandingPresentation.class);
+	
+	public void doProcess(){
+		
+		logger.debug("Processing");
+	}
+
+}
